c9e4377372e4fe1df8d3ed5b7710e10f299bd378
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-04 17:54:56 -0300

    feat: add MoviesList component with Movie Cards

---
+++ src/components/MoviesList/index.tsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import './index.scss';
+import { useState, useEffect } from 'react';
+import MovieCard from '../MovieCard';
+import { Movie } from '@/types/movie';
+import ReactLoading from 'react-loading';
+
+
+export default function MovieList() {
+    
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        getMovies();
+    }, [])
+
+    const getMovies = async () => {
+        const {data} = await axios.get('http://localhost:8080/movies');
+        setMovies(data);
+        setIsLoading(false);
+    }
+
+    if(isLoading){
+        return (
+            <div className="loading-container">
+                    <ReactLoading type="spin" color="#6046FF" height={'5%'} width={'5%'} />
+            </div>
+        )
+    }
+
+    return(
+        <ul className="movie-list">
+            
+            {movies.map((movie) => 
+            <MovieCard 
+                key={movie.id}
+                movie={movie}
+            />
+            )}
+            
+        </ul>
+    )
+}
