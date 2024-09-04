c99031544a17b3dc3fe5606b5cd77ad2cb7a760a
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-04 17:53:45 -0300

    feat: add StarRating component

---
+++ src/components/StarRating/index.tsx
@@ -0,0 +1,30 @@
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import './index.scss';
+
+export interface Props{
+    rating: number
+}
+
+export default function StarRating(props: Props){
+    const numStars = Math.round(props.rating / 2)
+
+    const fullStars = [];
+    const emptyStars = [];
+
+    for(let i = 0; i < 5; i++){
+        if(i < numStars){
+            fullStars.push(i);
+        }else{
+            emptyStars.push(i)
+        }
+    }
+
+    return(
+        <div className="movie-rate">
+            {fullStars.map(index => <FaStar key={index}/>)}
+            {emptyStars.map(index => <FaRegStar key={index}/>)}
+            
+        </div>
+    )
+}
