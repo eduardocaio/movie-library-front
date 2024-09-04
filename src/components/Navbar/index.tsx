d83680c12467e92206f41dc27be600829b755f34
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-04 17:51:51 -0300

    feat: add simple Navbar component and included it in the layout; made minor changes to global styling

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss';
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+        <h1 className='page-title'>Biblioteca de Filmes</h1>
+    </nav>
+  )
+}
