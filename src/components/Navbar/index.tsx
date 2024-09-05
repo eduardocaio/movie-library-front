c59efdf00a096331d7e8c0d2cd23dc7db69db554
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-05 11:53:04 -0300

    feat: add Header Component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './index.scss';
+import { MdFavorite } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-        <h1 className='page-title'>Biblioteca de Filmes</h1>
-    </nav>
+    <div className="header-container">
+      <h2 className="header-title">Biblioteca de Filmes</h2>
+      <div className="header-items">
+        <div className="header-item">Procurar</div>
+        <div className="header-item">Entrar</div>
+        <div className="header-item">Criar conta</div>
+        <div className="header-item"><MdFavorite size={30} /></div>
+      </div>
+    </div>
   )
 }
