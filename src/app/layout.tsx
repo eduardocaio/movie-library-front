fd7f0a2f3d9b6992b41f0a7abdc9be1689d6814f
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-05 12:57:03 -0300

    refactor: change project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import Navbar from "@/components/Navbar";
+import Home from "./Home/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <Home >{children}</Home>
+        
         </body>
     </html>
   );
