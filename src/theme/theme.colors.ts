3dc68252667f494c093c058de00a137f7a86d9e3
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-05 13:38:58 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,19 @@
+const Colors = {
+    primary: '',
+    text: {
+        white: '#F8F9FA',
+        dark: '#343A40'
+    },
+    background: {
+        dark: '#16181A',
+        shadow: '#4F4F4F'
+    },
+    input: {
+        background: '#E9ECEF',
+        placeholder: '#6C757D'
+    },
+    error: '#FF6A6A'
+
+}
+
+export default Colors;
