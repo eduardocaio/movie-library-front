64a0199198a7dfd6e73df56deec698c19c39a0c6
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-09 14:52:40 -0300

    refactor: refactor signup page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import { SignUpContainer, SignUpContent, SignUpHeadline, SignUpInputContainer } from "./signup.styled";
-import { FiLogIn } from "react-icons/fi";
 import { useForm } from "react-hook-form";
 import validator from "validator";
-import { Toast } from 'primereact/toast';
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import { FiLogIn } from "react-icons/fi";
 
 import CustomInput from "@/components/CustomInput";
 import CustomButton from "@/components/CustomButton";
 import InputErrorMessage from "@/components/InputErrorMessage";
 import { User } from "@/types/user";
 import { LoginService } from "@/services/LoginService";
+import CustomAlert from "@/components/CustomAlert";
+
+import './index.scss';
 
 interface SignupForm {
     name: string,
@@ -30,11 +32,21 @@ export default function Signup() {
         password: ''
     }
 
-    const [user, setUser] = useState<User>(userEmpty);
-    const loginService = useMemo(() => new LoginService, []);
-    const toast = useRef<Toast>(null);
+    const loginService = useMemo(() => new LoginService(), []);
     const { register, handleSubmit, watch, formState: { errors } } = useForm<SignupForm>();
     const watchPassword = watch('password');
+    const router = useRouter();
+    const [alertInfo, setAlertInfo] = useState<{ variant: string; message: string } | null>(null);
+
+    useEffect(() => {
+        if (alertInfo) {
+            const timer = setTimeout(() => {
+                setAlertInfo(null);
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [alertInfo]);
 
     const handleSubmitPress = (data: SignupForm) => {
         const newUser: User = {
@@ -44,105 +56,100 @@ export default function Signup() {
             password: data.password
         };
 
-        setUser(newUser);
 
-        console.log(newUser)
-        
+        setTimeout(() => {
+            router.refresh();
+        }, 500);
+
         loginService.newRegister(newUser).then((response) => {
-            setUser(userEmpty);
-            toast.current?.show({
-                severity: 'info',
-                summary: 'Sucesso!',
-                detail: 'Usuário cadastrado com sucesso!'
+            setAlertInfo({
+                variant: 'success',
+                message: 'Usuário registrado com sucesso!'
             });
         }).catch((error) => {
-            const errorMessage = error?.data?.message || 'Erro ao cadastrar!';
-            console.log(errorMessage);
-            toast.current?.show({
-                severity: 'error',
-                summary: 'Erro!',
-                detail: 'Erro ao cadastrar!'
-            })
+            const errorMessage = error?.response?.data?.message || 'Erro ao cadastrar!';
+            setAlertInfo({
+                variant: 'danger',
+                message: errorMessage
+            });
         });
 
     }
 
     return (
         <>
+            <div>
+                {alertInfo && <CustomAlert variant={alertInfo.variant} message={alertInfo.message} />}
+            </div>
 
-            <SignUpContainer>
-                <Toast ref={toast} />
-                <SignUpContent>
-                    <SignUpHeadline>Crie sua conta</SignUpHeadline>
+            <div className='signUpContainer'>
+                <div className='signUpContent'>
+                    <p className='signUpHeadline'>Crie sua conta</p>
 
-                    <SignUpInputContainer>
-
+                    <div className='signUpInputContainer'>
                         <p>Nome completo</p>
                         <CustomInput hasError={!!errors?.name} placeholder="Digite seu nome..." {...register('name', { required: true })} />
-
                         {errors?.name?.type === 'required' && (
-                            <InputErrorMessage >O nome completo é obrigatório</InputErrorMessage>
+                            <InputErrorMessage>O nome completo é obrigatório</InputErrorMessage>
                         )}
+                    </div>
 
-                    </SignUpInputContainer>
-
-                    <SignUpInputContainer>
-
+                    <div className='signUpInputContainer'>
                         <p>Nome de usuário</p>
                         <CustomInput hasError={!!errors?.username} placeholder="Digite seu nome de usuário..." {...register('username', { required: true })} />
-
                         {errors?.username?.type === 'required' && (
-                            <InputErrorMessage >O nome de usuário é obrigatório</InputErrorMessage>
+                            <InputErrorMessage>O nome de usuário é obrigatório</InputErrorMessage>
                         )}
+                    </div>
 
-                    </SignUpInputContainer>
-
-                    <SignUpInputContainer>
-
+                    <div className='signUpInputContainer'>
                         <p>E-mail</p>
-                        <CustomInput hasError={!!errors?.email} placeholder="Digite seu e-mail..." {...register('email', { required: true, validate: (value) => { return validator.isEmail(value) } })} />
-
+                        <CustomInput 
+                            hasError={!!errors?.email} 
+                            placeholder="Digite seu e-mail..." 
+                            {...register('email', { required: true, validate: (value) => validator.isEmail(value) })} 
+                        />
                         {errors?.email?.type === 'required' && (
-                            <InputErrorMessage >O e-mail é obrigatório</InputErrorMessage>
+                            <InputErrorMessage>O e-mail é obrigatório</InputErrorMessage>
                         )}
-
                         {errors?.email?.type === 'validate' && (
-                            <InputErrorMessage >Por favor, insira um e-mail válido</InputErrorMessage>
+                            <InputErrorMessage>Por favor, insira um e-mail válido</InputErrorMessage>
                         )}
+                    </div>
 
-                    </SignUpInputContainer>
-
-                    <SignUpInputContainer>
-
+                    <div className='signUpInputContainer'>
                         <p>Senha</p>
-                        <CustomInput hasError={!!errors?.password} placeholder="Digite sua senha..." type="password" {...register('password', { required: true })} />
-
+                        <CustomInput 
+                            hasError={!!errors?.password} 
+                            placeholder="Digite sua senha..." 
+                            type="password" 
+                            {...register('password', { required: true })} 
+                        />
                         {errors?.password?.type === 'required' && (
-                            <InputErrorMessage >A senha é obrigatória</InputErrorMessage>
+                            <InputErrorMessage>A senha é obrigatória</InputErrorMessage>
                         )}
+                    </div>
 
-                    </SignUpInputContainer>
-
-                    <SignUpInputContainer>
-
+                    <div className='signUpInputContainer'>
                         <p>Confirmação de senha</p>
-                        <CustomInput hasError={!!errors?.password} placeholder="Digite novamente sua senha..." type="password" {...register('confirmPassword', { required: true, validate: (value) => { return value === watchPassword } })} />
-
+                        <CustomInput 
+                            hasError={!!errors?.confirmPassword} 
+                            placeholder="Digite novamente sua senha..." 
+                            type="password" 
+                            {...register('confirmPassword', { required: true, validate: (value) => value === watchPassword })} 
+                        />
                         {errors?.confirmPassword?.type === 'required' && (
-                            <InputErrorMessage >É necessário fazer a confirmação da senha</InputErrorMessage>
+                            <InputErrorMessage>É necessário fazer a confirmação da senha</InputErrorMessage>
                         )}
-
                         {errors?.confirmPassword?.type === 'validate' && (
-                            <InputErrorMessage >As senhas não correspondem</InputErrorMessage>
+                            <InputErrorMessage>As senhas não correspondem</InputErrorMessage>
                         )}
-
-                    </SignUpInputContainer>
+                    </div>
 
                     <CustomButton startIcon={<FiLogIn size={18} />} onClick={() => handleSubmit(handleSubmitPress)()}>CRIAR CONTA</CustomButton>
 
-                </SignUpContent>
-            </SignUpContainer>
-
+                </div>
+            </div>
         </>
     )
 }
