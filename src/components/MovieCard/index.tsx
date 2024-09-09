05f5bf5a8f357c40407684b57f40e8ba77e2539b
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-09 14:48:52 -0300

    refactor: refactor MovieCard component

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,4 +1,6 @@
 import { Movie } from "@/types/movie"
+import { useRouter } from "next/navigation"
+
 import StarRating from "../StarRating"
 import './index.scss'
 
@@ -7,28 +9,39 @@ export interface Props {
 }
 
 export default function MovieCard(props: Props) {
-    const movie = props.movie
+    const { movie } = props;
+    const router = useRouter();
+
+    const handleMoreDetailsPress = () => {
+        router.push(`/movie/${movie.id}`);
+    }
+
     return (
-        <li className='movie-card'>
-
-            <div className="movie-poster">
-                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
-            </div>
-
-            <div className="movie-infos">
-                <p className="movie-title">{movie.title}</p>
-                <p>
-                    {movie.vote_average > 0 && <StarRating rating={movie.vote_average} />}
-                    
-                </p>
-                <div className="hidden-content">
-                    {movie.overview && <p className='description'>{movie.overview.length > 100 ? `${movie.overview.substring(0, 100)}...` : movie.overview}</p>}
-
-                    <button className="button-default">Ver mais</button>
-
+        <>
+            <li className='movie-card'>
+                <div className="movie-poster">
+                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
                 </div>
-            </div>
 
-        </li>
-    )
+                <div className="movie-infos">
+                    <p className="movie-title">{movie.title}</p>
+                    <p>
+                        {movie.vote_average > 0 && <StarRating rating={movie.vote_average} />}
+                    </p>
+                    <div className="hidden-content">
+                        {movie.overview && (
+                            <p className='description'>
+                                {movie.overview.length > 100 ? `${movie.overview.substring(0, 100)}...` : movie.overview}
+                            </p>
+                        )}
+
+                        <button className="button-default" onClick={handleMoreDetailsPress}>
+                            Ver mais
+                        </button>
+                    </div>
+                </div>
+            </li>
+
+        </>
+    );
 }
