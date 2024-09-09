bc5ebbc2bfc7f68a81b7bb63366f26b22b0d36c8
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-09 14:52:53 -0300

    refactor: refactor login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,84 +1,97 @@
 'use client'
 
-import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer } from "./login.styled";
-import { SlLogin } from "react-icons/sl";
 import { useForm } from "react-hook-form";
+import { useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import { SlLogin } from "react-icons/sl";
 
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
 import InputErrorMessage from "@/components/InputErrorMessage";
-import { useMemo, useRef } from "react";
+import CustomAlert from "@/components/CustomAlert";
 import { LoginService } from "@/services/LoginService";
-import { Toast } from "primereact/toast";
-import { useRouter } from "next/navigation";
 
-interface LoginForm{
+import './index.scss';
+
+interface LoginForm {
     username: string,
     password: string,
 }
 
-export default function Login () {
-
-    const {register, formState: {errors}, handleSubmit} = useForm<LoginForm>();
+export default function Login() {
+    const { register, formState: { errors }, handleSubmit } = useForm<LoginForm>();
     const loginService = useMemo(() => new LoginService(), [])
-    const toast = useRef<Toast>(null);
     const router = useRouter();
+    const [alertInfo, setAlertInfo] = useState<{ variant: string; message: string } | null>(null);
 
+    useEffect(() => {
+        if (alertInfo) {
+          const timer = setTimeout(() => {
+            setAlertInfo(null);
+          }, 5000);
+     
+          return () => clearTimeout(timer);
+        }
+      }, [alertInfo]);
 
     const handleSubmitPress = (data: any) => {
         loginService.login(data.username, data.password).then((response) => {
             localStorage.setItem('TOKEN_API_BACKEND', response.data.acessToken);
-            toast.current?.show({
-                severity: 'info',
-                summary: 'Sucesso!',
-                detail: 'Usuário logado com sucesso!'
+            setAlertInfo({
+                variant: 'success',
+                message: 'Logado com sucesso'
             });
+            window.location.reload()
 
-            router.push('/');
+            window.location.href = '/'
+
+            setTimeout(() => {
+                router.refresh();
+              }, 500);        
 
         }).catch((error) => {
-            const errorMessage = error?.data?.message || 'Erro ao cadastrar!';
-            console.log(errorMessage);
-            toast.current?.show({
-                severity: 'error',
-                summary: 'Erro!',
-                detail: 'Erro ao logar, usuário ou senha inválidos!'
-            })
+            const errorMessage = error?.response?.data?.message || 'Erro ao entrar na conta!';
+            setAlertInfo({
+                variant: 'danger',
+                message: errorMessage
+            });
         });
     }
 
-    return(
+    return (
         <>
-            <LoginContainer>
-                <Toast ref={toast} />
+            <div>
+                {alertInfo && <CustomAlert variant={alertInfo.variant} message={alertInfo.message} />}
+            </div>
+            <div className='loginContainer'>
 
-                <LoginContent>
+                <div className='loginContent'>
 
-                    <LoginHeadline>Entre com a sua conta</LoginHeadline>
-                
-                    <LoginInputContainer>
+                    <p className='loginHeadline'>Entre com a sua conta</p>
+
+                    <div className='loginInputContainer'>
                         <p>Nome de usuário</p>
-                        <CustomInput hasError={!!errors?.username} placeholder="Digite seu usuário..." {...register('username', {required: true})}/>
+                        <CustomInput hasError={!!errors?.username} placeholder="Digite seu usuário..." {...register('username', { required: true })} />
 
                         {errors?.username?.type === 'required' && (
                             <InputErrorMessage >O nome de usuário é obrigatório</InputErrorMessage>
                         )}
-                    </LoginInputContainer>
+                    </div>
 
-                    <LoginInputContainer>
+                    <div className='loginInputContainer'>
                         <p>Senha</p>
-                        <CustomInput hasError={!!errors?.password} placeholder="Digite sua senha..." {...register('password', {required: true})} type="password"/>
+                        <CustomInput hasError={!!errors?.password} placeholder="Digite sua senha..." {...register('password', { required: true })} type="password" />
 
                         {errors?.password?.type === 'required' && (
                             <InputErrorMessage >A senha é obrigatória</InputErrorMessage>
                         )}
-                    </LoginInputContainer>
+                    </div>
 
                     <CustomButton startIcon={<SlLogin size={20} />} onClick={() => handleSubmit(handleSubmitPress)()}>ENTRAR</CustomButton>
-                    
-                </LoginContent>
-                
-            </LoginContainer>
+
+                </div>
+
+            </div>
         </>
     )
 }
