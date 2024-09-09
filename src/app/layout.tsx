da2e5168bffd8179faff7b0fde7a51da3cdf7a3c
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-09 14:53:13 -0300

    refactor: refactor layout global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
-'use client'
+'use client';
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
-import Navbar from "@/components/Navbar";
-import Home from "./page";
 import { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+import Navbar from "@/components/Navbar";
+
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 const checkAuth = () => {
-  if(localStorage.getItem('TOKEN_API_BACKENDD') != undefined){
-    return true;
-  }else{
-    return false;
-  }
+  return localStorage.getItem('TOKEN_API_BACKEND') !== null;
 }
 
 export default function RootLayout({
@@ -24,19 +20,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const [pageLoaded, setPageLoaded] = useState(false);
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
     setAuthenticated(checkAuth());
-  })
+  }, []);
 
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Rede social de filmes" />
+        <title>CaJuFlix</title>
+      </Head>
       <body className={inter.className}>
         <Navbar checkAuth={authenticated}/>
         {children}
-        </body>
+      </body>
     </html>
   );
 }
