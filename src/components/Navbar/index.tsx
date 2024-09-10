cd00a70b1a4ad1c91457950ee375fe97a9ae7655
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-10 20:04:27 -0300

    feat: add logo in Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { FunctionComponent, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { jwtDecode } from 'jwt-decode';
@@ -11,7 +11,7 @@ import CustomInput from '../CustomInput';
 import CustomDropdown from '../Dropdown';
 
 interface SearchForm {
-  search: string
+  search: string;
 }
 
 interface DecodedToken {
@@ -38,47 +38,55 @@ const Navbar = () => {
   }
 
   const handleSubmitPress = (data: SearchForm) => {
-    if (!data.search) return
-    router.push(`/search/${data.search}`)
+    if (!data.search) return;
+    router.push(`/search/${data.search}`);
     reset({ search: '' });
   }
 
-
-
   return (
     <div className='header-container'>
-      <div className='header-title' onClick={handleHomeClick}>
-        CAJUFLIX
-      </div>
-
-      <div className='header-search'>
-        <CustomInput
-          className='form-control'
-          placeholder='Buscar por um filme...'
-          {...register('search')}
-          onEnterPress={handleSubmit(handleSubmitPress)}
+      <div className='header-left'>
+        <img 
+          src='/logo.png' 
+          alt='Logo' 
+          className='header-logo' 
+          onClick={handleHomeClick} // Adicione o manipulador de clique aqui
         />
+        <div className='header-title' onClick={handleHomeClick}>
+          CAJUFLIX
+        </div>
       </div>
 
-      {subject ? (
-        <CustomDropdown
-          menuItems={[
-            { label: 'Favoritos', action: 'favorites' },
-            { label: 'Sair', action: 'logout' },
-          ]}
-        >
-          {subject}
-        </CustomDropdown>
-      ) : (
-        <CustomDropdown
-          menuItems={[
-            { label: 'Entrar', action: 'login' },
-            { label: 'Criar Conta', action: 'signup' },
-          ]}
-        >
-          Iniciar sessão
-        </CustomDropdown>
-      )}
+      <div className='header-center'>
+        <div className='header-search'>
+          <CustomInput
+            className='form-control'
+            placeholder='Buscar por um filme...'
+            {...register('search')}
+            onEnterPress={handleSubmit(handleSubmitPress)}
+          />
+        </div>
+
+        {subject ? (
+          <CustomDropdown
+            menuItems={[
+              { label: 'Favoritos', action: 'favorites' },
+              { label: 'Sair', action: 'logout' },
+            ]}
+          >
+            {subject}
+          </CustomDropdown>
+        ) : (
+          <CustomDropdown
+            menuItems={[
+              { label: 'Entrar', action: 'login' },
+              { label: 'Criar Conta', action: 'signup' },
+            ]}
+          >
+            Iniciar sessão
+          </CustomDropdown>
+        )}
+      </div>
     </div>
   );
 }
