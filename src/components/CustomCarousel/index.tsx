aa0839c1f304b1b1a95b430fd5b4049e3bf46a90
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-11 17:02:31 -0300

    feat: add onClick in Carousel

--- src/components/CustomCarousel/index.tsx
+++ src/components/CustomCarousel/index.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Carousel from 'react-bootstrap/Carousel';
 import { Movie } from '@/types/movie';
+import { useRouter } from 'next/navigation';
 
 interface CustomCarouselProps {
     movies: Movie[];
 }
 
 function CustomCarousel({ movies }: CustomCarouselProps) {
+
+    const router = useRouter();
+
+    const handleMoreDetailsPress = (movieId: number) => {
+        router.push(`/movie/${movieId}`);
+    }
+
     return (
         <Carousel fade style={{ maxWidth: '100%', margin: '0 auto' }}>
             {movies.map((movie) => (
@@ -15,8 +23,10 @@ function CustomCarousel({ movies }: CustomCarouselProps) {
                     style={{
                         position: 'relative',
                         textAlign: 'center',
-                        marginTop: '30px'
+                        marginTop: '30px',
+                        cursor: 'pointer'
                     }}
+                    onClick={() => handleMoreDetailsPress(movie.id)}
                 >
                     <img
                         src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
