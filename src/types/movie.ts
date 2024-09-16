3f4b0a167fc59f0e6d4a1ab94b1b0a81d00886b2
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-16 18:38:33 -0300

    feat: add backdrop_path and actors in Movie interface

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,7 +1,11 @@
+import { Actor } from "./actor";
+
 export interface Movie{
     id: number,
     title: string,
     poster_path: string,
     overview: string,
-    vote_average: number
+    vote_average: number,
+    backdrop_path: string,
+    actors: Actor[];
 }
